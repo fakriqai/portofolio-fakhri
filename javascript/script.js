66e8a49b889be291b8cb8d9eff3528e6de74e41f
fakriqai <fakriqairawan7@gmail.com>
2025-01-06 20:05:30 +0700

    first commit about page

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+// Toogle
+const navbarNav = document.querySelector(".nav-menu");
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//
+const menu = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
